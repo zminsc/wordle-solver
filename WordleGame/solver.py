5ee6e176b48f021a01c2719045b243cf32a101fb
zminsc <zawmin.stevenchang@gmail.com>
2022-04-20 09:14:16 +0630

    Solver methods

--- WordleGame/solver.py
+++ WordleGame/solver.py
@@ -1,10 +1,24 @@
 class Solver:
     def __init__(self, game):
         self.game = game
+        self.frequency_table = {}
+        self.possible_guesses = game.valid_words
 
     def update(self, game):
         self.game = game
+        self.update_possible_guesses(game)
 
     def generate_guess(self):
         guess = "ADIEU"
         return guess
+
+    def update_frequency_table(self):
+        return {}
+
+    def assign_values(self, frequency_table):
+        # return values of each possible guess as a list
+        return []
+
+    def update_possible_guesses(self, game):
+        # go through the list of possible guesses and update it
+        return self.possible_guesses
