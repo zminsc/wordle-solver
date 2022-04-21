30145a173a993e30cc89f4bdb90c9711b3979a56
zminsc <zawmin.stevenchang@gmail.com>
2022-04-21 08:32:57 +0630

    frequency table function

--- WordleGame/solver.py
+++ WordleGame/solver.py
@@ -1,7 +1,6 @@
 class Solver:
     def __init__(self, game):
         self.game = game
-        self.frequency_table = {}
         self.possible_guesses = game.valid_words
 
     def update(self, game):
@@ -9,11 +8,21 @@ class Solver:
         self.update_possible_guesses(game)
 
     def generate_guess(self):
-        guess = "ADIEU"
-        return guess
+        if self.game.words_guessed == 0:
+            return "ADIEU"
+        else:
+            self.update_possible_guesses(self.game)
+            guess_values = self.assign_values(self.create_frequency_table())
+            return self.possible_guesses[guess_values.index(max(guess_values))]
 
-    def update_frequency_table(self):
-        return {}
+    def create_frequency_table(self):
+        frequency_table = {}
+        for i in range(0, 26):
+            frequency_table[chr(ord('A')+i)] = 0
+        for i in range(0, len(self.possible_guesses)):
+            for j in range(0, 5):
+                frequency_table[self.possible_guesses[i][j]] += 1
+        return frequency_table
 
     def assign_values(self, frequency_table):
         # return values of each possible guess as a list
