de00b53fb37be9b2b74987277dd1fa9abcae9ee6
zminsc <zawmin.stevenchang@gmail.com>
2022-04-28 09:43:43 +0630

    update main.py

--- main.py
+++ main.py
@@ -5,12 +5,21 @@ from WordleGame.utility import read_words
 # initialize the words
 word_list = read_words("WordleWordList.txt")
 
+games_solved = 0
+guesses_taken = []
+
 game = Game(word_list)
 solver = Solver(game)
 
-while not game.is_over():
-    game.display()
-    solver.update(game)
-    game.make_guess(solver.generate_guess())
+for i in range(1000):
+    game = Game(word_list)
+    solver = Solver(game)
+    while not game.is_over():
+        game.make_guess(solver.generate_guess())
+        solver.update(game)
+    if game.won:
+        guesses_taken.append(game.words_guessed)
+        games_solved += 1
 
-game.display()
+print("Solve Rate: " + str(games_solved/10) + "%")
+print("Average Guesses Taken: " + str(sum(guesses_taken)/len(guesses_taken)))
